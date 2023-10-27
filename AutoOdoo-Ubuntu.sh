78c20763d9ad3b41a0e57f76dbf7335df20564fe
gtoledano-losboscos <143846658+gtoledano-losboscos@users.noreply.github.com>
2023-10-27 18:36:51 +0200

    Fix "ask to continue" on pgAdmin installation

--- AutoOdoo-Ubuntu.sh
+++ AutoOdoo-Ubuntu.sh
@@ -68,7 +68,7 @@ sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '$POSTGRES_PASSWORD'
 # Instalación de pgAdmin4 ------------------------------------------------------------
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-sudo apt install pgadmin4-web
+sudo apt install pgadmin4-web -y
 
 # Instalación de Odoo ----------------------------------------------------------------
 wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
