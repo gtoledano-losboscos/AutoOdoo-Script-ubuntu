bbc2705ea8fb0c20f097d24f3a314155447d6062
gtoledano <guzman_toledano@hotmail.com>
2023-11-02 18:10:56 +0000

    Fix ask for confirmation on odoo installation

--- AutoOdoo-Ubuntu.sh
+++ AutoOdoo-Ubuntu.sh
@@ -80,7 +80,7 @@ sudo apt install pgadmin4-web -y
 sudo /usr/pgadmin4/bin/setup-web.sh -y
 
 # Instalación de Odoo ----------------------------------------------------------------
-wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
+wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/16.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
 sudo apt-get update && sudo apt-get install odoo -y
 
